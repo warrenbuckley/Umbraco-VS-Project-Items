fbd1f291bef9a0d677b88d6921c412097ef9595f
Warren Buckley <warren@creativewebspecialist.co.uk>
2013-10-11 15:02:20 +0100

    Added more template types (work in progress)

---
+++ Umbraco.VS.ProjectItems/Umbraco.VS.ProjectItems/ItemTemplates/Web/UmbracoApplicationEvents/RegisterEvents.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
+using Umbraco.Core.Publishing;
+using Umbraco.Core.Services;
+
+namespace $rootnamespace$
+{
+    public class  $safeitemrootname$ : ApplicationEventHandler
+    {
+
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ContentService.Published += ContentServiceOnPublished;
+        }
+
+        private void ContentServiceOnPublished(IPublishingStrategy sender, PublishEventArgs<IContent> publishEventArgs)
+        {
+            //For every node that has been published (may be single node, or node & it's children)
+            foreach (var node in publishEventArgs.PublishedEntities)
+            {
+                //If the node document type alias is Comment then...
+                if (node.ContentType.Alias == "Comment")
+                {
+                    //Pass comment to Spam Checker
+                    checkForSpam(node);
+                }
+            }
+        }
+    }
+}
